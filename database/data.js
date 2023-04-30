74e6545dd0af689b19d41dbea2c3a37689120fa9
anothersoham <122789751+anothersoham@users.noreply.github.com>
2023-04-30 21:22:32 +0100

    Add files via upload

---
+++ database/data.js
@@ -0,0 +1,104 @@
+export default [
+    {
+        id: 1,
+        question : "1. In 2023, how many types of residential tenancy agreements are there in the UK?",
+        options : [
+            'One',
+            'Three',
+            'Five',
+            'Six',
+        ]
+    },
+    {
+        id: 2,
+        question : "2. In England, a holding deposit cannot be more than:",
+        options : [
+            "a week’s rent.",
+            "a month’s rent.",
+            "two month’s rent.",
+            "a year’s rent.",
+        ]
+    },
+    {
+        id: 3,
+        question : "3. In England, if you pay for the holding deposit but fail to sign an agreement by the deadline, you are entitled for a refund within:",
+        options : [
+            'zero days',
+            'seven days',
+            'fourteen days',
+            'thirty days',
+        ]
+    },
+    {
+        id: 4,
+        question : "4. If you want to adopt a pet in your assured shorthold tenancy (AST) flat, can your landlord demand extra housing rent for that?",
+        options : [
+            'Yes',
+            'No',
+            'Yes, but only if the pet is not a dog and cat.',
+            'Yes, but only if you are not a senior citizen or differently abled.',
+        ]
+    },
+    {
+        id: 5,
+        question : "5. Despite you live in a hostel, dormitory, or hall of residence, you can still avail protection from eviction as a tenant if:",
+        options : [
+            'your landlord does not live in the same housing.',
+            'you have a lock on your door.',
+            'you have an agreement for a specific room.',
+            'all of the above.',
+        ]
+    },
+    {
+        id: 6,
+        question : "6. Are you legally bound to pay for a professional deep cleaning at the end of your tenancy?",
+        options : [
+            'Yes',
+            'No',
+            'Yes, but only if the agreement was signed during the COVID-19 pandemic.',
+            'Yes, but should only be deducted from the deposit.',
+        ]
+    },
+    {
+        id: 7,
+        question : "7. Suppose as a student, you want to end your fixed term tenancy contract because of discontinuing your study in the nearby college.  The landlord has denied agreeing but as you moved out, they welcomed in a new tenant. In this situation, they can legally sue you:",
+        options : [
+            'if deny paying the remaining rent in full.',
+            'if deny paying at least 30% of the remaining rent.',
+            'if fail to notify at least two weeks before evacuating.',
+            'never.',
+        ]
+    },
+    {
+        id: 8,
+        question : "8. Imagine you have been living in a property since last year. Now you are keen to extend the contract. But the landlord asks for a fee of £350 for the renewal of the license. You are required to pay:",
+        options : [
+            'the amount in full.',
+            'the amount in half.',
+            'a flat £100 towards administrative charges.',
+            'nothing.',
+        ]
+    },
+    {
+        id: 9,
+        question : "9. Assume you left your flat for a winter vacation and have forgotten to shut down the main water supply. As the temperature plummeted under 0°C, the pipeline burst.  Now, are you liable to cover the repairing cost?",
+        options : [
+            'No, because it happened in my absence.',
+            'Yes, but only need paying 50% of the cost incurred.',
+            'Yes, but only if stated earlier in the agreement.',
+            'Yes, but would only pay the amount stated on the invoice.',
+        ]
+    },
+    {
+        id: 10,
+        question : "10. If you have been served a valid Section 21 (No Fault) eviction notice, you can still stay at your accommodation for minimum:",
+        options : [
+            'a day.',
+            'a week.',
+            'a month.',
+            'two months.',
+        ]
+    }
+];
+
+export const answers = [3, 0, 0, 0, 3, 1, 3, 3, 3, 3];
